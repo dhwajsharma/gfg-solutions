bde42cb08f847c541e32c6afafe988d9d0f41847
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-07-30 19:00:40 +0530

    https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1

---
+++ Check for Balanced Tree.cpp
@@ -0,0 +1,33 @@
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    pair<bool, int> isBalancedFast(Node *root){
+        // base case
+        if(root == NULL){
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        pair<int,int> left = isBalancedFast(root->left);
+        pair<int,int> right = isBalancedFast(root->right);
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+      
+        bool diff = abs(left.second - right.second) <= 1;
+            
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff){
+            ans.first = true;
+        } else{
+            ans.second = false;
+        }
+        return ans;
+    }
+    
+    bool isBalanced(Node *root)
+    {
+        return isBalancedFast(root).first;
+    }
+};
